d5d4d5553c995f052a34ac56cedadffd5dce13ab
Kyo1006 <92916283+Kyo1006@users.noreply.github.com>
2022-03-18 12:43:54 +0700

    update main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from generation import Board
 from solveHung import *
 tk = Tk()
 cas = Canvas(tk, height = 600, width= 800)
-solv = Solve(cas = cas, n  = 4)
+solv = SolveHung(cas, 7)
 cas.pack()
-solv.solve()
+solv.solve((4,2))
 tk.mainloop()
