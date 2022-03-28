ebc45c69d8df41c8376fb703e20fa2124e63be01
Perovenus <69292119+perovenus@users.noreply.github.com>
2022-03-28 21:07:02 +0700

    abc

---
+++ .history/HCL_20220328210046.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from generation import Board
+from drawdomino import *
+from HillClimbing import *
+from time import *
+import tracemalloc
+tk = Tk()
+cas = Canvas(tk, height = 600, width= 800)
+solv = HillClimbing(cas = cas, n  = 3) 
+cas.pack()
+time_start = time()  # tính thời gian chạy, bắt đầu từ đây
+tracemalloc.start()
+solv.solve((0,0))
+current, peak = tracemalloc.get_traced_memory()
+print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
+tracemalloc.stop() 
+time_end = time()
+print("Run time: " + str(time_end - time_start))
+tk.mainloop()
